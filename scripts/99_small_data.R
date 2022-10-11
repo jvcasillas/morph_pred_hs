c533c568e1703bd26e76ee93c084a0e0e2c34264
Joseph V. Casillas <joseph.casillas@rutgers.edu>
2022-10-11 02:17:20 -0400

    rerun scripts and knit

--- scripts/99_small_data.R
+++ scripts/99_small_data.R
@@ -12,6 +12,7 @@
 source(here::here("scripts", "04_load_data.R"))
 prof_use <- read_csv(here("data", "tidy", "prof_use.csv"))
 
+# Load model tibbles
 prof_use_desc             <- read_csv(here("tables", "tab_prof_use_desc.csv"))
 prof_use_mod              <- read_csv(here("tables", "tab_prof_use_mod.csv"))
 group_gamm_tib            <- read_csv(here("tables", "tab_group_gamm_mod.csv"))
@@ -19,6 +20,8 @@ group_prediction_tib      <- read_csv(here("tables", "tab_group_prediction.csv")
 group_marginal_slopes_tib <- read_csv(here("tables", "tab_group_marginal_slopes.csv"))
 bi_gamm_tib               <- read_csv(here("tables", "tab_bi_gamm_mod.csv"))
 bi_zoid_tib               <- read_csv(here("tables", "tab_bi_zoid_mod.csv"))
+bi_zoid_marginals_tib     <- read_csv(here("tables", "tab_bi_zoid_marginals.csv")) 
+
 
 # -----------------------------------------------------------------------------
 
